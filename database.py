3fb24043c2f591ce769a1cd8c33da6bea8ba7bdb
Juan Jose Angamarca <jangamarcac1@est.ups.edu.ec>
2024-06-15 00:59:21 -0500

    Cambio para despliegue

--- database.py
+++ database.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-URL_DATABASE = "mysql+pymysql://root:@localhost:3306/marketbd"
-engine = create_engine(URL_DATABASE)
+
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./market.db" 
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base= declarative_base()
-
-
-
+Base = declarative_base()
--- dockerfile
+++ dockerfile
@@ -1,7 +1,6 @@
-FROM python
+FROM python:3.8-slim
 WORKDIR /app
-COPY requirements.txt .
-RUN pip install -r requirements.txt
 COPY . .
-EXPOSE 5000
-CMD ["python", "main.py"]
+RUN pip install --no-cache-dir -r requirements.txt
+EXPOSE 8000
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
