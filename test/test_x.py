2a06bb2dd93cc7b5f919cadf32c34b72bbe92204
ERLEOSUS <esuarezl@est.ups.edu.ec>
2024-06-17 02:51:36 -0500

    test_prov

--- test/test_x.py
+++ test/test_x.py
@@ -14,3 +14,19 @@ def test_crear_usuario(client):
     }
     response = client.post("/clientes/crear_cliente/", json=new_user)
     assert response.status_code == 201  # Corregido aquí, elimina el espacio en blanco
+
+def test_crear_proveedor(client):
+    new_prov ={
+        "nombre": "Proveedor de prueba",
+        "contacto": "Contacto de prueba",
+        "productos": "Productos de prueba",
+        "condiciones_pago": "Condiciones pago  de prueba"
+    }
+    response=client.post("/proveedores/registro_proveedor/", json=new_prov)
+    assert response.status_code == 201
+
+def test_listar_proveedores(client):
+    response = client.get("/proveedores/listado_proveedores/")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
